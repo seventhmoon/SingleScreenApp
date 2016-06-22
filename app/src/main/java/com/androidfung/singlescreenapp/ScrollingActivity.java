0481e66db4bc46363f913a3ffd4572d966da0d2f
seventhmoon <funglam@7thmoon.com>
2016-06-23 01:49:03 +0800

    Initial commit

---
+++ app/src/main/java/com/androidfung/singlescreenapp/ScrollingActivity.java
@@ -0,0 +1,40 @@
+package com.androidfung.singlescreenapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ImageView imageViewHeader = (ImageView) findViewById(R.id.image_header);
+        Glide.with(this).load(R.drawable.googleplex).centerCrop().into(imageViewHeader);
+
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
